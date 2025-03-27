16423d67ef174dbbdf283fee818c4a14da43a58a
OlehKniaziev <atmatm9182@gmail.com>
2025-03-27 21:05:37 +0100

    Lab2 tasks completed :)

--- src/dom.ts
+++ src/dom.ts
@@ -1,5 +1,5 @@
-import type { Project, ProjectUpdateParams } from "./storage";
-import { globalProjectRepository } from "./storage";
+import type { Feature, FeaturePriority, FeatureState, Project, User, ProjectUpdateParams } from "./storage";
+import { globalProjectRepository, globalUserRepository, globalFeatureRepository, createNewFeatureRightNow } from "./storage";
 
 type NamedEventListener = {
     event: string;
@@ -11,12 +11,19 @@ type DynamicElement = {
     listeners: NamedEventListener[];
 };
 
+const mainHeader = querySelectorMust("#main-header");
+
 const messageBox = querySelectorMust("#message-box");
 
 const creationForm = querySelectorMust("#creation-form");
 const updateForm = querySelectorMust("#update-form");
 
 const projectListElem = querySelectorMust("#project-list");
+
+const activeProjectElem = querySelectorMust("#active-project");
+const activeProjectFeatureListElem = querySelectorMust("#active-project-feature-list");
+
+const newFeatureForm = querySelectorMust("#new-feature-form");
 
 const dynamicElements: DynamicElement[] = [
     {
@@ -37,7 +44,15 @@ const dynamicElements: DynamicElement[] = [
             },
         ],
     },
-
+    {
+        el: newFeatureForm,
+        listeners: [
+            {
+                event: "submit",
+                listener: newFeatureFormOnSubmit,
+            },
+        ],
+    }
 ];
 
 function querySelectorMust(selector: string): Element {
@@ -50,12 +65,17 @@ function querySelectorMust(selector: string): Element {
 }
 
 function getFormInputValue(form: HTMLFormElement, inputName: string): string {
-    const input = form.elements.namedItem(inputName) as HTMLInputElement | null;
+    const input = form.elements.namedItem(inputName);
     if (!input) {
-        throw new Error(`Could not find a named item with name '${name}'`);
+        throw new Error(`Could not find a named item with name '${inputName}'`);
     }
 
-    return input.value;
+    if (input instanceof HTMLInputElement)
+        return input.value;
+    if (input instanceof HTMLSelectElement)
+        return input.value;
+
+    throw new Error(`Unsupported form input: ${input}`);
 };
 
 function creationFormOnSubmit(ev: Event) {
@@ -100,10 +120,43 @@ function updateFormOnSubmit(ev: Event) {
         description: projectDescription,
     };
 
-    if (newProjectName === "") delete updateParams.newName;
+    if (newProjectName === "")     delete updateParams.newName;
     if (projectDescription === "") delete updateParams.description;
 
     globalProjectRepository.update(updateParams);
+}
+
+function newFeatureFormOnSubmit(ev: Event) {
+    const event = ev as SubmitEvent;
+
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement | undefined;
+    if (!form) {
+        throw new Error("The event target is null!");
+    }
+
+    const featureName = getFormInputValue(form, "name");
+    const featureDescription = getFormInputValue(form, "description");
+    const featurePriority = getFormInputValue(form, "priority");
+
+    const activeProject = globalProjectRepository.getActive();
+    if (!activeProject) throw new Error("No currently active project");
+    const activeUser = globalUserRepository.getActive();
+    if (!activeUser) throw new Error("No currently active user");
+
+    if (featurePriority !== "low" && featurePriority !== "medium" && featurePriority !== "high")
+        throw new Error(`Unsupported feature priority ${featurePriority}`);
+
+    const newFeature = createNewFeatureRightNow({
+        name: featureName,
+        description: featureDescription,
+        priority: featurePriority,
+        owner: activeUser,
+        project: activeProject,
+    });
+
+    globalFeatureRepository.addFeature(newFeature);
 }
 
 type MessageLevel = "info" | "error";
@@ -127,24 +180,264 @@ function projectToElem(proj: Project): HTMLElement {
     descriptionP.textContent = proj.description;
 
     const removeButton = document.createElement("button");
-    removeButton.classList.add("button-danger");
+    removeButton.classList.add("button", "button-danger");
 
     removeButton.textContent = "Remove";
 
     removeButton.addEventListener("click", () => globalProjectRepository.deleteByName(proj.name));
 
-    mainDiv.append(idP, nameP, descriptionP, removeButton);
+    const setCurrentButton = document.createElement("button");
+    setCurrentButton.classList.add("button", "button-action");
+
+    setCurrentButton.textContent = "Set as current project";
+
+    setCurrentButton.addEventListener("click", () => globalProjectRepository.setActive(proj));
+
+    if (proj.id === globalProjectRepository.getActive()?.id) {
+        const activeText = document.createElement("b");
+        activeText.textContent = "[ACTIVE]";
+        mainDiv.appendChild(activeText);
+    }
+
+    mainDiv.append(idP, nameP, descriptionP, removeButton, setCurrentButton);
 
     return mainDiv;
 }
 
-export function initState() {
-    const projects = globalProjectRepository.queryAll();
+function updateActiveProjectViewHook(proj: Project): void {
+    const table = document.createElement("table");
+    const tHead = document.createElement("thead");
+    const tBody = document.createElement("tbody");
 
-    for (const proj of projects) {
-        const projAsElem = projectToElem(proj);
-        projectListElem.appendChild(projAsElem);
+    const headerRow = document.createElement("tr");
+
+    const hId = document.createElement("th");
+    const hName = document.createElement("th");
+    const hDescr = document.createElement("th");
+
+    hId.textContent = "Id";
+    hName.textContent = "Name";
+    hDescr.textContent = "Description";
+
+    headerRow.append(hId, hName, hDescr);
+    tHead.appendChild(headerRow);
+
+    const bodyRow = document.createElement("tr");
+
+    const idCell = document.createElement("td");
+    const nameCell = document.createElement("td");
+    const descrCell = document.createElement("td");
+
+    idCell.textContent = proj.id;
+    nameCell.textContent = proj.name;
+    descrCell.textContent = proj.description;
+
+    bodyRow.append(idCell, nameCell, descrCell);
+    tBody.appendChild(bodyRow);
+
+    table.append(tHead, tBody);
+
+    activeProjectElem.replaceChildren(table);
+}
+
+function updateMainHeaderHook(newUser: User): void {
+    mainHeader.textContent = `Currently logged in as ${newUser.firstName} ${newUser.lastName}.`;
+}
+
+function showPopup(...children: HTMLElement[]): void {
+    const target = document.body;
+
+    const dialog = document.createElement("dialog");
+
+    const closeButton = document.createElement("button");
+    closeButton.classList.add("button");
+
+    closeButton.textContent = "Close";
+
+    closeButton.addEventListener("click", () => {
+        dialog.close();
+        target.removeChild(dialog);
+    });
+
+    dialog.append(...children, closeButton);
+
+    target.appendChild(dialog);
+
+    dialog.showModal();
+}
+
+function displayFeatureDetails(feat: Feature): void {
+    const featureElem = document.createElement("div");
+
+    const title = document.createElement("h1");
+    title.textContent = "Feature details";
+
+    const p = (field: keyof Feature): HTMLElement => {
+        const e = document.createElement("p");
+        const fieldValue = feat[field] as string;
+        e.textContent = `${field}: ${fieldValue}`;
+        return e;
+    };
+
+    const pId = p("id");
+    const pName = p("name");
+    const pDescription = p("description");
+    const pCreationDate = p("creationDate");
+
+    const prioritySelect = document.createElement("select");
+
+    const prio = (value: FeaturePriority): void => {
+        const opt = document.createElement("option");
+        opt.value = value;
+        opt.textContent = value;
+        if (feat.priority === value) opt.selected = true;
+        prioritySelect.appendChild(opt);
+    };
+
+    prio("low");
+    prio("medium");
+    prio("high");
+
+    prioritySelect.addEventListener("change", () => {
+        feat.priority = prioritySelect.value as FeaturePriority;
+        globalFeatureRepository.updateFeature(feat);
+    });
+
+    const project = globalProjectRepository.queryByID(feat.projectID);
+    if (!project) throw new Error(`No project with id ${feat.projectID}`);
+
+    const pProject = document.createElement("p");
+    pProject.textContent = `projectID: ${feat.projectID}(${project.name})`;
+
+    const owner = globalUserRepository.queryByID(feat.ownerID);
+    if (!owner) throw new Error(`No user with id ${feat.ownerID}`);
+
+    const pOwner = document.createElement("p");
+    pOwner.textContent = `ownerID: ${feat.ownerID}(${owner.firstName} ${owner.lastName})`;
+
+    const stateSelect = document.createElement("select");
+
+    const state = (value: FeatureState): void => {
+        const opt = document.createElement("option");
+        opt.value = value;
+        opt.textContent = value;
+        if (feat.state === value) opt.selected = true;
+        stateSelect.appendChild(opt);
+    };
+
+    state("todo");
+    state("in-progress");
+    state("done");
+
+    stateSelect.addEventListener("change", () => {
+        feat.state = stateSelect.value as FeatureState;
+        globalFeatureRepository.updateFeature(feat);
+
+        const currentProject = globalProjectRepository.getActive()!;
+        renderProjectFeatures(currentProject);
+    });
+
+    featureElem.append(
+        pId,
+        pName,
+        pDescription,
+        pCreationDate,
+        pProject,
+        pOwner,
+        prioritySelect,
+        stateSelect,
+    );
+
+    showPopup(featureElem);
+}
+
+function renderProjectFeatures(proj: Project): void {
+    const features = globalFeatureRepository.getProjectFeatures(proj);
+
+    const table = document.createElement("table");
+    const tHead = document.createElement("thead");
+    const tBody = document.createElement("tbody");
+
+    const todos = [];
+    const doings = [];
+    const dones = [];
+
+    for (const feature of features) {
+        switch (feature.state) {
+            case "todo": todos.push(feature); break;
+            case "in-progress": doings.push(feature); break;
+            case "done": dones.push(feature); break;
+        }
     }
+
+    const maxLength = Math.max(todos.length, doings.length, dones.length);
+
+    const appendFeatureIfExists = (row: HTMLElement, feature: Feature | undefined) => {
+        const cell = document.createElement("td");
+        if (feature) {
+            cell.textContent = feature.name;
+            cell.classList.add("pointer");
+            cell.addEventListener("click", () => displayFeatureDetails(feature));
+        }
+        row.appendChild(cell);
+    };
+
+    for (let i = 0; i < maxLength; ++i) {
+        const row = document.createElement("tr");
+        appendFeatureIfExists(row, todos[i]);
+        appendFeatureIfExists(row, doings[i]);
+        appendFeatureIfExists(row, dones[i]);
+        tBody.appendChild(row);
+    }
+
+    const hTodo = document.createElement("th");
+    hTodo.textContent = "todo";
+    const hDoing = document.createElement("th");
+    hDoing.textContent = "doing";
+    const hDone = document.createElement("th");
+    hDone.textContent = "done";
+
+    tHead.append(hTodo, hDoing, hDone);
+
+    table.append(tHead, tBody);
+
+    activeProjectFeatureListElem.replaceChildren(table);
+}
+
+function renderAllProjects() {
+    const projects = globalProjectRepository.queryAll();
+    const projectsElements = projects.map(projectToElem);
+    projectListElem.replaceChildren(...projectsElements);
+}
+
+export function initState() {
+    renderAllProjects();
+
+    const currentUser = globalUserRepository.getActive();
+    updateMainHeaderHook(currentUser);
+
+    const currentProject = globalProjectRepository.getActive();
+    // FIXME
+    if (currentProject) {
+        updateActiveProjectViewHook(currentProject);
+        renderProjectFeatures(currentProject);
+    }
+
+    globalUserRepository.attachEventHook({
+        type: "active-user-changed",
+    }, updateMainHeaderHook);
+
+    globalProjectRepository.attachEventHook({
+        type: "active-project-changed",
+    }, updateActiveProjectViewHook);
+
+    globalProjectRepository.attachEventHook({
+        type: "active-project-changed",
+    }, () => renderAllProjects());
+
+    globalProjectRepository.attachEventHook({
+        type: "active-project-changed",
+    }, renderProjectFeatures);
 }
 
 export function setupEventListeners() {
--- src/storage.ts
+++ src/storage.ts
@@ -10,15 +10,92 @@ export type ProjectUpdateParams = {
     description?: string | undefined;
 };
 
+type ProjectEventSelector = {
+    type: "active-project-changed";
+};
+
+type ProjectEventHook = (proj: Project) => void;
+
 interface ProjectRepository {
     createWithRandomID(name: string, description: string): Project;
     queryAll(): Project[];
     queryByName(name: string): Project | null;
+    queryByID(id: string): Project | null;
     deleteByName(name: string): void;
+    deleteByID(id: string): void;
     update(params: ProjectUpdateParams): void;
+    setActive(project: Project): void;
+    getActive(): Project | undefined;
+    attachEventHook(selector: ProjectEventSelector, hook: ProjectEventHook): void;
+}
+
+type ProjectRepositoryMutator = {
+    setActiveProject(proj: Project): void;
+    addProject(proj: Project): void;
+    removeProject(projectID: string): void;
+};
+
+class ProjectRepositoryState {
+    private activeProject?: Project;
+    private projects: Project[] = [];
+
+    public constructor() {
+        const existingState = localStorage.getItem(ProjectRepositoryState.LOCAL_STORAGE_KEY);
+
+        if (!existingState) {
+            const thisString = JSON.stringify(this);
+            localStorage.setItem(ProjectRepositoryState.LOCAL_STORAGE_KEY, thisString);
+        } else {
+            const state = JSON.parse(existingState) as ProjectRepositoryState;
+            this.activeProject = state.activeProject;
+            this.projects = state.projects;
+        }
+    }
+
+    public modify(cb: (mutator: ProjectRepositoryMutator) => void): void {
+        const state = this;
+
+        const mutator: ProjectRepositoryMutator = {
+            setActiveProject(proj: Project): void {
+                state.activeProject = proj;
+            },
+            addProject(proj: Project): void {
+                state.projects.push(proj);
+            },
+            removeProject(projectID: string): void {
+                for (let i = 0; i < state.projects.length; ++i) {
+                    if (state.projects[i].id === projectID) {
+                        state.projects.splice(i, 1);
+                        return;
+                    }
+                }
+
+                throw new Error(`No project with id '${projectID}' found`);
+            },
+        };
+
+        cb(mutator);
+
+        const stateString = JSON.stringify(state);
+        localStorage.setItem(ProjectRepositoryState.LOCAL_STORAGE_KEY, stateString);
+    }
+
+    public queryAll(): Project[] {
+        return this.projects;
+    }
+
+    public getActive(): Project | undefined {
+        return this.activeProject;
+    }
+
+    private static LOCAL_STORAGE_KEY = "project-repository";
+
 }
 
 class LocalStorageProjectRepository implements ProjectRepository {
+    private state: ProjectRepositoryState = new ProjectRepositoryState();
+    private activeProjectChangedHooks: ProjectEventHook[] = [];
+
     public createWithRandomID(name: string, description: string): Project {
         const id = crypto.randomUUID();
         const proj: Project = {
@@ -33,19 +110,24 @@ class LocalStorageProjectRepository implements ProjectRepository {
     }
 
     public queryAll(): Project[] {
-        const projects = [];
+        return this.state.queryAll();
+    }
 
-        for (const projectName of this.projectNames()) {
-            const projJSON = localStorage.getItem(projectName)!;
-            const proj = JSON.parse(projJSON);
-            projects.push(proj);
-        }
-
-        return projects;
+    public deleteByID(id: string): void {
+        this.state.modify((mutator) => mutator.removeProject(id));
     }
 
     public deleteByName(name: string): void {
-        localStorage.removeItem(name);
+        const projects = this.queryAll();
+
+        for (const project of projects) {
+            if (project.name === name) {
+                this.deleteByID(project.id);
+                return;
+            }
+        }
+
+        throw new Error(`No project with name '${name}' found`);
     }
 
     public update({ oldName, newName, description }: ProjectUpdateParams ): void {
@@ -54,40 +136,292 @@ class LocalStorageProjectRepository implements ProjectRepository {
             throw new Error(`Cannot find a project with name '${oldName}'`);
         }
 
+        this.deleteByName(oldName);
+
         if (newName !== undefined)     proj.name = newName;
         if (description !== undefined) proj.description = description;
 
-        this.deleteByName(oldName);
-
-        const projString = JSON.stringify(proj);
-        localStorage.setItem(proj.name, projString);
+        this.saveProject(proj);
     }
 
     public queryByName(name: string): Project | null {
-        const projectString = localStorage.getItem(name);
-        if (projectString !== null) return JSON.parse(projectString);
+        const projects = this.queryAll();
+
+        for (const project of projects) {
+            if (project.name === name) return project;
+        }
+
         return null;
     }
 
-    private saveProject(proj: Project): void {
-        const serialized = JSON.stringify(proj);
+    public queryByID(id: string): Project | null {
+        const projects = this.queryAll();
 
-        if (localStorage.getItem(proj.name) !== null) {
-            throw new Error(`Cannot create a project: name '${proj.name}' is already taken`);
+        for (const project of projects) {
+            if (project.id === id) return project;
         }
 
-        localStorage.setItem(proj.name, serialized);
+        return null;
     }
 
-    private *projectNames(): Generator<string> {
-        for (let i = 0; i < localStorage.length; i++) {
-            yield localStorage.key(i)!;
+    public setActive(project: Project): void {
+        this.state.modify((mutator) => mutator.setActiveProject(project));
+
+        for (const hook of this.activeProjectChangedHooks) hook(project);
+    }
+
+    public getActive(): Project | undefined {
+        return this.state.getActive();
+    }
+
+    public attachEventHook(selector: ProjectEventSelector, hook: ProjectEventHook): void {
+        switch (selector.type) {
+            case "active-project-changed":
+                this.activeProjectChangedHooks.push(hook);
+                break;
+        }
+    }
+
+    private saveProject(proj: Project): void {
+        this.state.modify((mutator) => mutator.addProject(proj));
+    }
+}
+
+export type User = Readonly<{
+    id: string;
+    firstName: string;
+    lastName: string;
+}>;
+
+export type UserEventSelector = {
+    type: "active-user-changed";
+};
+
+export type UserEventHook = (user: User) => void;
+
+interface UserRepository {
+    queryByID(id: string): User | null;
+    getActive(): User;
+    setActiveUser(user: User): void;
+    attachEventHook(selector: UserEventSelector, hook: UserEventHook): void;
+}
+
+type UserRepositoryMutator = {
+    setActiveUser(user: User): void;
+    addUser(user: User): void;
+};
+
+class UserRepositoryState {
+    private users: User[] = [];
+    private activeUser: User;
+
+    constructor() {
+        const existingState = localStorage.getItem(UserRepositoryState.LOCAL_STORAGE_KEY);
+
+        if (!existingState) {
+            this.activeUser = adminUser;
+            this.users.push(this.activeUser);
+
+            const thisString = JSON.stringify(this);
+            localStorage.setItem(UserRepositoryState.LOCAL_STORAGE_KEY, thisString);
+        } else {
+            const state = JSON.parse(existingState) as UserRepositoryState;
+
+            this.activeUser = state.activeUser;
+            this.users = state.users;
+        }
+    }
+
+    public modify(cb: (mutator: UserRepositoryMutator) => void): void {
+        const repositoryState = this;
+
+        const mutator: UserRepositoryMutator = {
+            setActiveUser(user: User): void {
+                repositoryState.activeUser = user;
+            },
+
+            addUser(user: User): void {
+                repositoryState.users.push(user);
+            },
+        };
+
+        cb(mutator);
+
+        const stateJSONString = JSON.stringify(this);
+        localStorage.setItem(UserRepositoryState.LOCAL_STORAGE_KEY, stateJSONString);
+    }
+
+    public queryByID(id: string): User | null {
+        for (const user of this.users) {
+            if (user.id === id) return user;
+        }
+
+        return null;
+    }
+
+    public getActive(): User {
+        return this.activeUser;
+    }
+
+    private static LOCAL_STORAGE_KEY: string = "user-repository";
+}
+
+class LocalStorageUserRepository implements UserRepository {
+    private state: UserRepositoryState;
+    private activeUserChangedHooks: UserEventHook[] = [];
+
+    constructor() {
+        this.state = new UserRepositoryState();
+    }
+
+    public getActive(): User {
+        return this.state.getActive();
+    }
+
+    public queryByID(id: string) {
+        return this.state.queryByID(id);
+    }
+
+    public setActiveUser(user: User): void {
+        this.state.modify((mutator) => mutator.setActiveUser(user));
+
+        for (const hook of this.activeUserChangedHooks) {
+            hook(user);
+        }
+    }
+
+    public attachEventHook(selector: UserEventSelector, hook: UserEventHook): void {
+        switch (selector.type) {
+            case "active-user-changed":
+                this.activeUserChangedHooks.push(hook);
+                break;
         }
     }
 }
 
+export type FeaturePriority = "low" | "medium" | "high";
+
+export type FeatureState = "todo" | "in-progress" | "done";
+
+export type Feature = {
+    id: string;
+    name: string;
+    description: string;
+    priority: FeaturePriority;
+    projectID: string;
+    creationDate: string;
+    ownerID: string;
+    state: FeatureState;
+};
+
+export function createNewFeatureRightNow(
+    {
+        name,
+        description,
+        priority,
+        owner,
+        project,
+    }: {
+        name: string,
+        description: string,
+        priority: FeaturePriority,
+        owner: User,
+        project: Project,
+    },
+): Feature {
+    const creationDate = (new Date()).toISOString();
+
+    return {
+        id: crypto.randomUUID(),
+        ownerID: owner.id,
+        projectID: project.id,
+        state: "todo",
+        name,
+        description,
+        priority,
+        creationDate,
+    };
+}
+
+export interface FeatureRepository {
+    addFeature(feat: Feature): void;
+    getProjectFeatures(proj: Project): Feature[];
+    updateFeature(feat: Feature): void;
+};
+
+type FeatureRepositoryMutator = {
+    addFeature(feat: Feature): void;
+    updateFeature(feat: Feature): void;
+};
+
+class FeatureRepositoryState {
+    private static LOCAL_STORAGE_KEY = "feature-repository";
+
+    private features: Feature[] = [];
+
+    public constructor() {
+        const existingState = localStorage.getItem(FeatureRepositoryState.LOCAL_STORAGE_KEY);
+
+        if (!existingState) {
+            const thisString = JSON.stringify(this);
+            localStorage.setItem(FeatureRepositoryState.LOCAL_STORAGE_KEY, thisString);
+        } else {
+            const state = JSON.parse(existingState) as FeatureRepositoryState;
+            this.features = state.features;
+        }
+    }
+
+    public modify(cb: (mutator: FeatureRepositoryMutator) => void): void {
+        const state = this;
+
+        const mutator: FeatureRepositoryMutator = {
+            addFeature(feat: Feature): void {
+                state.features.push(feat);
+            },
+            updateFeature(feat: Feature): void {
+                feat;
+            },
+        };
+
+        cb(mutator);
+
+        const stateString = JSON.stringify(state);
+        localStorage.setItem(FeatureRepositoryState.LOCAL_STORAGE_KEY, stateString);
+    }
+
+    public getProjectFeatures(proj: Project): Feature[] {
+        return this.features.filter((feat) => feat.projectID === proj.id);
+    }
+}
+
+class LocalStorageFeatureRepository implements FeatureRepository {
+    private state: FeatureRepositoryState = new FeatureRepositoryState();
+
+    public addFeature(feat: Feature): void {
+        this.state.modify((mutator) => mutator.addFeature(feat));
+    }
+
+    public getProjectFeatures(proj: Project): Feature[] {
+        return this.state.getProjectFeatures(proj);
+    }
+
+    public updateFeature(feature: Feature): void {
+        this.state.modify((mutator) => mutator.updateFeature(feature));
+    }
+};
+
+const adminUser: User = {
+    id: "admin-id",
+    firstName: "admin",
+    lastName: "adminenko",
+};
+
 const globalProjectRepository: ProjectRepository = new LocalStorageProjectRepository();
+const globalUserRepository: UserRepository = new LocalStorageUserRepository();
+const globalFeatureRepository: FeatureRepository = new LocalStorageFeatureRepository();
 
 export {
     globalProjectRepository,
+    globalUserRepository,
+    globalFeatureRepository,
 };
